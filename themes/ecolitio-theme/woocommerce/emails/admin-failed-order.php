c5f0f5577c465dbd902335d7eac29a9c38eed466
skLuan <luan.erazo@skyrocket.com.co>
2026-02-21 02:55:46 +0000

    new images and emails template changes

--- themes/ecolitio-theme/woocommerce/emails/admin-failed-order.php
+++ themes/ecolitio-theme/woocommerce/emails/admin-failed-order.php
@@ -34,7 +34,7 @@ echo $email_improvements_enabled ? '<div class="email-introduction">' : '';
 $text = __( 'Payment for order #%1$s from %2$s has failed. The order was as follows:', 'woocommerce' );
 if ( $email_improvements_enabled ) {
 	/* translators: %1$s: Order number. %2$s: Customer full name. */
-	$text = __( 'Unfortunately, the payment for order #%1$s from %2$s has failed. The order was as follows:', 'woocommerce' );
+	$text = __( 'Desafortunadamente, el pago para la orden #%1$s de %2$s ha fallado. Resumen de la orden:', 'woocommerce' );
 }
 ?>
 <p><?php printf( esc_html( $text ), esc_html( $order->get_order_number() ), esc_html( $order->get_formatted_billing_full_name() ) ); ?></p>
